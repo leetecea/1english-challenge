89b42a4bfd79744993f10deb12068435794ee010
leetecea <leeleticiac@gmail.com>
2022-06-30 13:34:47 -0300

    challenge finished

---
+++ script.js
@@ -0,0 +1,25 @@
+let comedy = [
+    {title: "Central Intelligence"},
+    {director:"Rawson Marshall Thurber"},
+    {year: 2016},
+]
+
+let action = [
+    {title: "Mad Max: Fury Road"},
+    {director:"George Miller"},
+    {year: 2015},
+]
+
+let fantasy = [
+    {title: "Pan's Labyrinth"},
+    {director:"Guillermo del Toro"},
+    {year: 2006},
+]
+
+let favoriteMovie =  "Pan's Labyrinth";
+
+if(favoriteMovie ===  fantasy[0].title){
+    console.log("My favorite movie is Pan's Labyrinth");
+}else{
+    console.log("Not even 5 stars...");
+}
